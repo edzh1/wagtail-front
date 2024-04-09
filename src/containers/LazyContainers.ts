221a53b87a8a014142aeb5afddbeb99e5408b35f
Dmitrii Kovtun <dkovtun.work@gmail.com>
2024-04-09 19:28:14 +0300

    home page

--- src/containers/LazyContainers.ts
+++ src/containers/LazyContainers.ts
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 
 const containers = {
+    HomePage: dynamic(() => import('./HomePage/HomePage')),
     AboutPage: dynamic(() => import('./AboutPage/AboutPage')),
     NotFoundPage: dynamic(() => import('./NotFoundPage/NotFoundPage')),
 }
